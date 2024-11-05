0c551cf33808d6af1c1b7f6c3ae1c268ea770255
Camilla Mai Ryskjær <CARY@zealand.dk>
2024-11-05 04:59:07 +0100

    Add project files.

---
+++ Dragracing-Simple/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Dragracing_Simple
+{
+    internal class Car
+    {
+        public int Number { get; set; }
+        public double Speed { get; private set; } // Meter per sekund
+        public double Distance { get; private set; } // Meter
+
+        public long Time { get; private set; }    
+
+        public event Action<Car> OnFinish; // Event for at signalere, når bilen er færdig
+    
+        public Car(int number)
+        {
+            Number = number;
+            Speed = 0;
+            Distance = 0;
+        }
+
+        public void StartRace()
+        {
+            long starttid = (long)(DateTime.Now.TimeOfDay.TotalMilliseconds);
+            while (Distance < 400) // Løb indtil 400 meter
+            {
+                // Øg hastigheden med 5 m/s
+                Speed += 5;
+                // Beregn den tilbagelagte afstand
+                Distance += Speed * 0.5; // 0.5 sekunder
+
+                // Skriv hastighed og afstand til konsollen
+                Console.WriteLine($"Nummer: {Number} Hastighed: {Speed} m/s, Tilbagelagt afstand: {Distance} meter");
+
+                // Vent i 500 ms
+                Thread.Sleep(500);
+            }
+            Time = (long)(DateTime.Now.TimeOfDay.TotalMilliseconds)-starttid;
+            
+            OnFinish?.Invoke(this);
+           
+           
+        }
+    }
+
+}
+
---
+++ Dragracing-Simple/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Xml.Linq;
+
+namespace Dragracing_Simple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car raceCar1 = new Car(1);
+            Thread raceThread = new Thread(raceCar1.StartRace);
+
+            Car raceCar2 = new Car(2);
+            Thread raceThread1 = new Thread(raceCar2.StartRace);
+
+            raceCar2.OnFinish += RaceCar_OnFinish;
+            raceCar1.OnFinish += RaceCar_OnFinish;
+
+            // Start racet
+            raceThread.Start();
+            // Vent på, at racet afsluttes
+           
+
+            raceThread1.Start();
+            // Vent på, at racet afsluttes
+            raceThread1.Join();
+
+
+            raceThread.Join();
+            Console.WriteLine("Løbet er afsluttet");
+            Console.ReadKey();
+        }
+
+        private static void RaceCar_OnFinish(Car c)
+        {
+            Console.WriteLine($"{c.Number} har gennemført løbet på: "+c.Time +" ms.");
+        }
+    }
+}
